346ab645c6ca8386e9571133bae8ce586957b401
xpavelf <you@example.com>
2017-02-09 15:01:21 +0100

    sidenav btn in toolbar

--- client/src/components/Toolbar.jsx
+++ client/src/components/Toolbar.jsx
@@ -3,16 +3,16 @@ import { Link } from "react-router-dom"
 import imgLogo from "../icons/logo.png"
 import SearchBox  from "./SearchBox"
 
-// <Link className="Toolbar__sideNavLink" to={ ({state: {sideNav: true}}) }>
-//   <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 24"><path d="M0 0h24v24H0z" fill="none"/><path d="M3 18h18v-2H3v2zm0-5h18v-2H3v2zm0-7v2h18V6H3z" fill="#FFF"/></svg>
-// </Link>
+
 
 export default class Toolbar extends React.Component {
   render() {
     return (
       <header className="Toolbar">
         <h1 className="Toolbar__title">
-
+          <Link className="Toolbar__sideNavLink" to={ ({state: {sideNav: true}}) }>
+            <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 24"><path d="M0 0h24v24H0z" fill="none"/><path d="M3 18h18v-2H3v2zm0-5h18v-2H3v2zm0-7v2h18V6H3z" fill="#FFF"/></svg>
+          </Link>
           <Link to="/" className="Toolbar__homeLink"><img alt="Logo - viscokupujes.cz" src={imgLogo} /></Link>
           <a target="_blank" rel="noopener" href="https://www.facebook.com/viscokupujes" title="Facebook" className="Toolbar__fbLink">
             <svg height="32" width="32" viewBox="0 0 512 512">
