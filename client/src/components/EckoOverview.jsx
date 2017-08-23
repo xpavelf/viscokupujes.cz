818dc9b6428a396eef81ec0fcc1c80ace58746c3
xpavelf <you@example.com>
2017-08-23 10:50:56 +0200

    ecko overview filter

--- client/src/components/EckoOverview.jsx
+++ client/src/components/EckoOverview.jsx
@@ -2,13 +2,37 @@ import React from "react"
 import ecka from "../../../e.json"
 import { eComparator } from "../utils/eUtils"
 import EckoCard from "./EckoCard"
+import { remove as removeDiacritics } from "diacritics"
+import "./EckoOverview.css"
 
-export default (props) => {
-  let eArr = Object.keys(ecka)
-    .reduce((acc, e) => acc.concat({...ecka[e], id: e}), [])
-    .sort(eComparator)
+const ECKO_ARR = Object.keys(ecka)
+  .reduce((acc, e) => acc.concat({...ecka[e], id: e}), [])
+  .map(e => Object.assign({}, e, { searchStr: removeDiacritics([e.id, ...e.names].join(',').toLowerCase()) }))
+  .sort(eComparator)
 
-  return (
-    <div>{ eArr.map(e => <EckoCard key={e.id} e={e} />) } </div>
-  )
+export default class EckoOverview extends React.Component {
+
+  state = {
+    term: ''
+  }
+
+  onChange = (e) => {
+    this.setState({ term: removeDiacritics(e.target.value.trim().toLowerCase()) })
+  }
+
+  render() {
+    let eArr = ECKO_ARR.filter(e => e.id.indexOf(this.state.term) !== -1 || e.searchStr.indexOf(this.state.term) !== -1)
+    return (
+      <div>
+        <div className="EckoOverview__filter">
+          <div>Hledej éčko.</div>
+          <input onChange={this.onChange} />
+        </div>
+        { eArr.length
+            ? eArr.map(e => <EckoCard key={e.id} e={e} />)
+            : <div className="EckoOverview__filterEmpty">Hledanému výrazu neodpovídá žádné éčko.</div>
+        }
+      </div>
+    )
+  }
 }
