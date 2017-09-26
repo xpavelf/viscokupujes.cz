2651899ec279359636e2eda7f1144d1e6ab044a6
xpavelf <you@example.com>
2017-09-26 11:54:05 +0200

    MessageBus: msg footer support

--- client/src/components/MessageBus.jsx
+++ client/src/components/MessageBus.jsx
@@ -1,27 +1,30 @@
 import React from "react"
 import "./MessageBus.css"
 import { connect } from "react-redux"
-const SHOW_MSG_INTERVAL = 5000
+const SHOW_MSG_INTERVAL = 6000
 
 @connect((store) => ({ messages: store.messages }))
 export default class MessageBus extends React.Component {
-  state = {}
+  state = { hide: false }
 
   componentWillReceiveProps(nextProps) {
     let msg = nextProps.messages[0]
-    this.setState({ msg })
-    setTimeout(() => this.setState({ msg: null }), SHOW_MSG_INTERVAL)
+    this.setState({ msg, hide: false })
+    setTimeout(this.hide, SHOW_MSG_INTERVAL)
   }
+
+  hide = () => this.setState({ hide: true })
 
   render() {
     let msg = this.state.msg
-    let cls = "MessageBus" + (msg ? " MessageBus--visible" : "")
+    let cls = "MessageBus" + (msg && !this.state.hide ? " MessageBus--visible" : "")
     return (
       <div className={cls}>
         { msg
             ? <div>
                 <div className="MessageBus__title">{msg.title}</div>
                 <div className="MessageBus__text">{msg.text}</div>
+                {msg.getFooter ? msg.getFooter(this.hide) : null}
               </div>
             : null
         }
