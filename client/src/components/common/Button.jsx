73a0d7102e1cc24f82ed492b2af902c9eb5073f7
xpavelf <you@example.com>
2017-11-27 13:37:54 +0100

    common/Button: new size - small

--- client/src/components/common/Button.jsx
+++ client/src/components/common/Button.jsx
@@ -2,8 +2,11 @@ import React from "react"
 import "./Button.css"
 
 export default (props) => {
-  let clazz = "VisCoKupujesBtn" + (props.color ? " VisCoKupujesBtn--" + props.color : "")
-  let { color, className, ...p } = props
+  let clazz = "VisCoKupujesBtn"
+    + (props.color ? " VisCoKupujesBtn--" + props.color : "")
+    + (props.size ? " VisCoKupujesBtn--" + props.size : "")
+
+  let { color, size, className, ...p } = props
   return (
     <button className={clazz + (className ? ` ${className}` : "")} {...p}>
       {props.children}
