d0cfdf2fa2f56cbc64958d38ee055e285b8c2981
xpavelf <you@example.com>
2018-01-24 00:16:03 +0100

    fixes for cordova 7

--- build.sh
+++ build.sh
@@ -2,6 +2,7 @@
 
 XWALK_PLUGIN_NAME=cordova-plugin-crosswalk-webview
 APP_ID=cz.viscokupujes.mnamka
+OUT_APK_FOLDER=platforms/android/app/build/outputs/apk
 
 function _isPluginInstalled {
   cordova plugin ls | grep "$1"
@@ -19,7 +20,7 @@ function frontend {
 }
 
 function releaseOld {
-  cordova plugin add cordova-plugin-crosswalk-webview
+  cordova plugin add https://github.com/crosswalk-project/cordova-plugin-crosswalk-webview#2.4.0
   _release
 }
 
@@ -32,30 +33,30 @@ function _release {
   local ver=$(_getApkVersion ./config.xml)
   local version=${ver}
 
-  rm -rf platforms/android/build/outputs/apk/*
+  rm -rf ${OUT_APK_FOLDER}/*
   if (_isPluginInstalled $XWALK_PLUGIN_NAME) ; then
-    cordova build android --release
+    cordova build android --release -- --gradleArg=-PcdvBuildMultipleApks=true --gradleArg=-PcdvBuildArch=arm
     version=${ver}-xwalk
-    mv platforms/android/build/outputs/apk/android-armv7-release-unsigned.apk platforms/android/build/outputs/apk/android-release-unsigned.apk
+    mv ${OUT_APK_FOLDER}/armv7/release/app-armv7-release-unsigned.apk ${OUT_APK_FOLDER}/release/app-release-unsigned.apk
   else
     version=${ver}4
     cordova build android --release -- --minSdkVersion=21 --versionCode=${version}
   fi
 
-  pushd platforms/android/build/outputs/apk
-  zipalign -f 4 android-release-unsigned.apk "${APP_ID}-${version}-unsigned.aligned.apk"
+  pushd ${OUT_APK_FOLDER}/release
+  zipalign -f 4 app-release-unsigned.apk "${APP_ID}-${version}-unsigned.aligned.apk"
   apksigner.bat sign --ks /d/dev/keys/my-release-key.jks --out "${APP_ID}-${version}.apk" "${APP_ID}-${version}-unsigned.aligned.apk"
   popd
-  mv "platforms/android/build/outputs/apk/${APP_ID}-${version}.apk" apk/
+  mv "${OUT_APK_FOLDER}/release/${APP_ID}-${version}.apk" apk/
 
 }
 
 function test {
   cordova build android
   if (_isPluginInstalled $XWALK_PLUGIN_NAME) ; then
-    adb install -r platforms/android/build/outputs/apk/android-armv7-debug.apk
+    adb install -r ${OUT_APK_FOLDER}/debug/app-debug.apk
   else
-    adb install -r platforms/android/build/outputs/apk/android-debug.apk
+    adb install -r ${OUT_APK_FOLDER}/debug/app-debug.apk
   fi
   adb shell monkey -p ${APP_ID} -c android.intent.category.LAUNCHER 1
 }
