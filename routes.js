44e32937e665bd6ed0ed625796436abff913ccb0
xpavelf <you@example.com>
2019-12-21 16:09:40 +0100

    search in unapproved products as well

--- routes.js
+++ routes.js
@@ -6,6 +6,8 @@ const { ecka, getAdditives } = require('ecka')
 const { hasGlukoseSirup, hasPalmOil, populateE } = require('./productUtils')
 const { queryStringSearch } = require("./strUtils")
 
+const USER_PRODUCTS_PATH = `${__dirname}/user-products`
+
 module.exports = (app) => {
 
   const predicateWithHits = (query) => (prod) => {
@@ -36,6 +38,42 @@ module.exports = (app) => {
     return name ? arr_filter_with_map(products, predicateWithHits(name), limit) : products.slice(0, limit)
   }
 
+  const getUnapprovedFromFile = (file) => {
+    if (file) {
+      let product = JSON.parse(fs.readFileSync(`${USER_PRODUCTS_PATH}/review/${file}`))
+      product.id = `FILE-${Buffer.from(file).toString('base64')}`
+
+      let p = {
+        bc: product.ean,
+        ingredients: product.ing,
+        id: product.id,
+        producer: product.producer,
+        name: 'POZOR ZATÍM NESCHVÁLENO: ' + product.name,
+        e: getAdditives(product.ing),
+        a: getAllergens(product.ing),
+        po: hasPalmOil(product.ing),
+        gf: hasGlukoseSirup(product.ing),
+        promProducts: []
+      }
+
+      if (p.name.length < 4 || p.ingredients.length < 5)
+        return null
+
+      return populateE(p)
+    }
+  
+    return null
+  }
+
+  const searchInUnapproved = (bc) => {
+    let file = fs.readdirSync(`${USER_PRODUCTS_PATH}/review`)
+      .find(f => f.endsWith(".json") && f.slice(15).slice(0, -5) === ""+bc)
+
+    return getUnapprovedFromFile(file)
+
+    return null
+  }
+
   app.post("/api/report", (req, res) => {
     let d = new Date()
     let dstr = `${d.getDate()}.${d.getMonth() + 1}.${d.getFullYear()} ${d.getHours()}:${('0' + d.getMinutes()).slice(-2)}`
@@ -52,10 +90,18 @@ module.exports = (app) => {
     if (bc) {
       let bcInt = parseInt(bc)
       retVal = searchByBc(bcInt)
+      
       if (retVal) {
         res.json(populateE(retVal))
       } else {
-        res.status(404).json(null)
+        retVal = searchInUnapproved(bcInt)
+        if (retVal) {
+          
+          res.json(retVal)
+        } else {
+          res.status(404).json(null)
+        }
+        
       }
     } else if (name) {
       retVal = searchByName(name)
@@ -66,6 +112,13 @@ module.exports = (app) => {
   app.get("/api/product/:id", (req, res) => {
     let id = req.params.id
     console.info("product get by id %s", id)
+
+    if (id.startsWith('FILE-')) {
+      let idbase64 = id.slice(5)
+      let filename = Buffer.from(idbase64, 'base64').toString('ascii')
+      let p = getUnapprovedFromFile(filename)
+      return res.json(p)
+    }
 
     const product = products.find(product => product.id === id)
 
