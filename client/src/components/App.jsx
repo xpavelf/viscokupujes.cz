5b947b9dc36c019368ff9d0b7065ec25e6d8c852
xpavelf <you@example.com>
2020-02-10 16:24:58 +0100

    App.jsx refactor to hooks

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,10 +1,10 @@
-import React from "react"
+import React, { useEffect } from "react"
 import {
   Toolbar,
   SideNav,
   MessageBus,
 } from "@components"
-import { Switch, Route, withRouter, Redirect, Link } from "react-router-dom"
+import { Switch, Route, useLocation, Redirect, Link } from "react-router-dom"
 import Product from "../pages/Product"
 import EckoOverview from "../pages/EckoOverview"
 import Home from "../pages/Home"
@@ -17,60 +17,55 @@ import imgSprite from "../icons/sprite.svg"
 import "./Header.css"
 import "./App.css"
 
-@withRouter
-export default class App extends React.Component {
+export default function App() {
+  const pathname = useLocation().pathname
 
-  componentWillReceiveProps(nextProps) {
-    let page = nextProps.location.pathname
-    if (this.props.location.pathname !== page) {
-      if (ga) {
-        ga('set', 'page', page)
-        ga('send', 'pageview')
-      }
-
-      setDataPage(page)
+  useEffect(() => {
+    if (ga) {
+      ga('set', 'page', pathname)
+      ga('send', 'pageview')
     }
-  }
+    setDataPage(pathname)
+  }, [pathname])
 
-  render() {
-    return (
-      <main className="App">
-        <SideNav />
-        <header className="Header">
-          <Link className="Header__homeLink" to="/">
-            <div className="Header__title">
-              <svg height="90" width="90"><use xlinkHref={`${imgSprite}#logo-wo-circle`}></use></svg>
-              Víš co kupuj<span className="Header__titleE">E</span>š?
-            </div>
-            <div className="Header__subtitle">Najdi si éčka v potravinách a nekupuj zbytečnou chemii</div>
-          </Link>
-
-
-          <Toolbar />
-          <div style={{ maxWidth: 760, margin: "0 auto", position: "relative" }}>
-            <Link className="Header__sideNavLink" to={ ({state: {sideNav: true}}) }>
-              <svg width="40" height="40" style={{ color: "white" }}><use xlinkHref={`${imgSprite}#menu`}></use></svg>
-            </Link>
+  return (
+    <main className="App">
+      <SideNav />
+      <header className="Header">
+        <Link className="Header__homeLink" to="/">
+          <div className="Header__title">
+            <svg height="90" width="90"><use xlinkHref={`${imgSprite}#logo-wo-circle`}></use></svg>
+            Víš co kupuj<span className="Header__titleE">E</span>š?
           </div>
-        </header>
+          <div className="Header__subtitle">Najdi si éčka v potravinách a nekupuj zbytečnou chemii</div>
+        </Link>
 
 
-        <div className="App__content">
-          <Switch>
-            <Route exact path="/" component={Home} />
-            <Route exact path="/android_asset/www/index.html" component={Home} />
-            <Redirect from="/add-product/entry" to="/add-product" />
-            <Route path="/add-product" component={AddProduct} />
-            <Route path="/product/:id" component={Product} />
-            <Route path="/ecka" component={EckoOverview} />
-            <Route path="/search-history" component={SearchHistory} />
-            <Route path="/user-product/approve" component={ApproveUserProduct} />
-            <Route path="/alert" component={Alert} />
-            <Redirect from="/*" to="/" />
-          </Switch>
+        <Toolbar />
+        <div style={{ maxWidth: 760, margin: "0 auto", position: "relative" }}>
+          <Link className="Header__sideNavLink" to={ ({state: {sideNav: true}}) }>
+            <svg width="40" height="40" style={{ color: "white" }}><use xlinkHref={`${imgSprite}#menu`}></use></svg>
+          </Link>
         </div>
-        <MessageBus />
-      </main>
-    )
-  }
+      </header>
+
+
+      <div className="App__content">
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/android_asset/www/index.html" component={Home} />
+          <Redirect from="/add-product/entry" to="/add-product" />
+          <Route path="/add-product" component={AddProduct} />
+          <Route path="/product/:id" component={Product} />
+          <Route path="/ecka" component={EckoOverview} />
+          <Route path="/search-history" component={SearchHistory} />
+          <Route path="/user-product/approve" component={ApproveUserProduct} />
+          <Route path="/alert" component={Alert} />
+          <Redirect from="/*" to="/" />
+        </Switch>
+      </div>
+      <MessageBus />
+    </main>
+  )
 }
+
