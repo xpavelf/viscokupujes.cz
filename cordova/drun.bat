43497b8a2519a7683dd2d974994a025469ad7783
xpavelf <youS@example.com>
2021-03-01 19:40:08 +0100

    upg

--- cordova/drun.bat
+++ cordova/drun.bat
@@ -1,1 +1,1 @@
-docker run -it --rm -v %cd%:/app -v %cd%\..\www:/app/www -v %cd%\..\..\keys:/keys:ro -w /app --name vck c %*
+docker run -it -v %cd%:/app -v %cd%\..\www:/app/www -v %cd%\..\..\keys:/keys:ro -w /app --name vck c %*
